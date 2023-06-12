491edb7ab4efe7f78bb6fa6754d2d04f2804ec75
RutvikKansagara <rutvikkansagara7852@gmail.com>
2023-06-12 18:21:16 +0530

    second commit

---
+++ app/Http/Controllers/AdminUserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminUserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json($users);
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+
+$data = $request->all();
+$data['password'] = bcrypt($request->input('password'));
+//dd($data);
+User::create($data);
+
+        return response()->json(['message' => 'New User has been created'], 200);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json(['message' => 'user deleted successfully']);
+    }
+}
